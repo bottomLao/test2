f7db234da14dabe3eb05f269316fc17c22a2e425
hz <hezhe@nxin.com>
2016-10-14 13:57:38 +0800

    test2

---
+++ test2/Test2.java
@@ -0,0 +1,16 @@
+package test2;
+
+/**
+ * Created by hz on 2016/10/14.
+ */
+public class Test2 {
+    private String test;
+
+    public String getTest() {
+        return test;
+    }
+
+    public void setTest(String test) {
+        this.test = test;
+    }
+}
